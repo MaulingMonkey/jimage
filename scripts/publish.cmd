0c32af684d003d40ebf64e61d430cdfcbf40f7c1
MaulingMonkey <git@maulingmonkey.com>
2020-01-15 04:22:04 -0800

    scripts\publish.cmd:  Add 10s delay, fix publish order

--- scripts/publish.cmd
+++ scripts/publish.cmd
@@ -1,5 +1,6 @@
 @pushd "%~dp0.."
-cd "%~dp0..\jimage"     && cargo publish %* || goto :err
 cd "%~dp0..\jimage-sys" && cargo publish %* || goto :err
+@ping localhost -n 11 >NUL 2>NUl
+cd "%~dp0..\jimage"     && cargo publish %* || goto :err
 :err
 @popd && exit /b %ERRORLEVEL%
